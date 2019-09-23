3c2bed573c7f194bfa618fea1261167afe7dddec
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2019-09-23 07:42:42 +0530

    Add files via upload

---
+++ pairs.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pairs function below.
+    
+    static int pairs(int k, int[] arr) {
+    int sum =0;
+    Set<Integer> set = new HashSet<Integer>();
+    for(int n=0; n< arr.length; n++){
+        set.add(arr[n]);
+    }
+    for(int n=0; n< arr.length; n++){
+        if(set.contains(arr[n]+k)){
+            sum+=1;
+        }
+    }
+    return sum;
+}
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
